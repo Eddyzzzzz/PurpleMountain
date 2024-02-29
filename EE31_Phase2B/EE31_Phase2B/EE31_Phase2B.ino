a0cff5225cfd06e887cf5e33ec28ba28ded994b4
Eddyzzzzz <eddyzyf@gmail.com>
2024-02-29 16:26:55 -0500

    Update Feb 29

--- EE31_Phase2B/EE31_Phase2B/EE31_Phase2B.ino
+++ EE31_Phase2B/EE31_Phase2B/EE31_Phase2B.ino
@@ -1,13 +1,9 @@
-int enablePinA = 5;
-int enablePinB = 6;
-int in1 = 9;
-int in2 = 10;
-int in3 = 11;
-int in4 = 12;
+int in1 = 6;
+int in2 = 5;
+int in3 = 10;
+int in4 = 11;
 
 void setup() {
-  pinMode(enablePinA, OUTPUT);
-  pinMode(enablePinB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
@@ -16,31 +12,27 @@ void setup() {
 
 void loop() {
   // Move forward
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(enablePinA, 255); // full speed
-  analogWrite(enablePinB, 255); // full speed
+  analogWrite(in1, 155);
+  analogWrite(in2, 0);
+  analogWrite(in3, 155);
+  analogWrite(in4, 0);
   delay(2000); // Move for 2 seconds
 
   // Stop
-  analogWrite(enablePinA, 0);
-  analogWrite(enablePinB, 0);
+  analogWrite(in1, 0);
+  analogWrite(in3, 0);
   delay(1000); // Stop for 1 second
 
-  // Turn right
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(enablePinA, 150); // reduce speed to 78%
-  analogWrite(enablePinB, 150);
-  delay(1000); // Turn for 1 second
+  // Turn left
+  analogWrite(in1, 0);
+  analogWrite(in2, 110);
+  analogWrite(in3, 110);
+  analogWrite(in4, 0);
+  delay(1000); // Turn for 2 second
 
   // Stop
-  analogWrite(enablePinA, 0);
-  analogWrite(enablePinB, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
   delay(1000); // Stop for 1 second
 
 }
